d6717e2a1dcf2622305f4fbfcb34c62ad409d983
Jonathan Miller <jonathan_miller@Jonathans-MacBook-Air.local>
2021-01-08 09:09:16 +0000

    who_dunnit

--- who_dunnit_hmwk.js
+++ who_dunnit_hmwk.js
@@ -1,146 +1,146 @@
 // Episode 1
 
-// const scenario = {
-//     murderer: 'Miss Scarlet',
-//     room: 'Library',
-//     weapon: 'Rope'
-//   };
+const scenario = {
+    murderer: 'Miss Scarlet',
+    room: 'Library',
+    weapon: 'Rope'
+  };
   
-//   const declareMurderer = function() {
-//     return `The murderer is ${scenario.murderer}.`;
-//   }
+  const declareMurderer = function() {
+    return `The murderer is ${scenario.murderer}.`;
+  }
   
-//   const verdict = declareMurderer();
-//   console.log(verdict);
+  const verdict = declareMurderer();
+  console.log(verdict);
 
 // // I believe the murderer would be 'Miss Scarlet'
 
 // // Episode 2
 
-// const murderer = 'Professor Plum';
+const murderer = 'Professor Plum';
 
-// const changeMurderer = function() {
-//   murderer = 'Mrs. Peacock';
-// }
+const changeMurderer = function() {
+  murderer = 'Mrs. Peacock';
+}
 
-// const declareMurderer = function() {
-//   return `The murderer is ${murderer}.`;
-// }
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
 
-// changeMurderer();
-// const verdict = declareMurderer();
-// console.log(verdict);
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
 
 // // I believe the murderer would be 'Professor Plum as you cannot change a const.
 
 // // Episode 3
 
-// let murderer = 'Professor Plum';
+let murderer = 'Professor Plum';
 
-// const declareMurderer = function() {
-//   let murderer = 'Mrs. Peacock';
-//   return `The murderer is ${murderer}.`;
-// }
+const declareMurderer = function() {
+  let murderer = 'Mrs. Peacock';
+  return `The murderer is ${murderer}.`;
+}
 
-// const firstVerdict = declareMurderer();
-// console.log('First Verdict: ', firstVerdict);
+const firstVerdict = declareMurderer();
+console.log('First Verdict: ', firstVerdict);
 
-// const secondVerdict = `The murderer is ${murderer}.`;
-// console.log('Second Verdict: ', secondVerdict);
+const secondVerdict = `The murderer is ${murderer}.`;
+console.log('Second Verdict: ', secondVerdict);
 
 // // I believe the firstVerdict would return 'Mrs Peacock' as the murderer.
 // // The second verdict would return 'Professor Plum' as the changed murderer only has block scope.
 
 // // Episode 4
 
-// let suspectOne = 'Miss Scarlet';
-// let suspectTwo = 'Professor Plum';
-// let suspectThree = 'Mrs. Peacock';
+let suspectOne = 'Miss Scarlet';
+let suspectTwo = 'Professor Plum';
+let suspectThree = 'Mrs. Peacock';
 
-// const declareAllSuspects = function() {
-//   let suspectThree = 'Colonel Mustard';
-//   return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
-// }
+const declareAllSuspects = function() {
+  let suspectThree = 'Colonel Mustard';
+  return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+}
 
-// const suspects = declareAllSuspects();
-// console.log(suspects);
-// console.log(`Suspect three is ${suspectThree}.`);
+const suspects = declareAllSuspects();
+console.log(suspects);
+console.log(`Suspect three is ${suspectThree}.`);
 
 // // I believe suspect three would 'Colonel Mustard' as it has been changed in the function that is being called on.
 
 // // Episode 5 
 
-// const scenario = {
-//     murderer: 'Miss Scarlet',
-//     room: 'Kitchen',
-//     weapon: 'Candle Stick'
-//   };
+const scenario = {
+    murderer: 'Miss Scarlet',
+    room: 'Kitchen',
+    weapon: 'Candle Stick'
+  };
   
-//   const changeWeapon = function(newWeapon) {
-//     scenario.weapon = newWeapon;
-//   }
+  const changeWeapon = function(newWeapon) {
+    scenario.weapon = newWeapon;
+  }
   
-//   const declareWeapon = function() {
-//     return `The weapon is the ${scenario.weapon}.`;
-//   }
+  const declareWeapon = function() {
+    return `The weapon is the ${scenario.weapon}.`;
+  }
   
-//   changeWeapon('Revolver');
-//   const verdict = declareWeapon();
-//   console.log(verdict);
+  changeWeapon('Revolver');
+  const verdict = declareWeapon();
+  console.log(verdict);
 
 // // I believe it would still be the revolver as the contents of the const has been changed with the two new functions.
 
 // // Episode 6
 
-// let murderer = 'Colonel Mustard';
+let murderer = 'Colonel Mustard';
 
-// const changeMurderer = function() {
-//   murderer = 'Mr. Green';
+const changeMurderer = function() {
+  murderer = 'Mr. Green';
 
-//   const plotTwist = function() {
-//     murderer = 'Mrs. White';
-//   }
+  const plotTwist = function() {
+    murderer = 'Mrs. White';
+  }
 
-//   plotTwist();
-// }
+  plotTwist();
+}
 
-// const declareMurderer = function () {
-//   return `The murderer is ${murderer}.`;
-// }
+const declareMurderer = function () {
+  return `The murderer is ${murderer}.`;
+}
 
-// changeMurderer();
-// const verdict = declareMurderer();
-// console.log(verdict);
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
 
 // I believe the murderer would be 'Mrs White' as the plotTwist() changes the murderer and is in the correct scope.
 
 // Episode 7
 
-// let murderer = 'Professor Plum';
+let murderer = 'Professor Plum';
 
-// const changeMurderer = function() {
-//   murderer = 'Mr. Green';
+const changeMurderer = function() {
+  murderer = 'Mr. Green';
 
-//   const plotTwist = function() {
-//     let murderer = 'Colonel Mustard';
+  const plotTwist = function() {
+    let murderer = 'Colonel Mustard';
 
-//     const unexpectedOutcome = function() {
-//       murderer = 'Miss Scarlet';
-//     }
+    const unexpectedOutcome = function() {
+      murderer = 'Miss Scarlet';
+    }
 
-//     unexpectedOutcome();
-//   }
+    unexpectedOutcome();
+  }
 
-//   plotTwist();
-// }
+  plotTwist();
+}
 
-// const declareMurderer = function() {
-//   return `The murderer is ${murderer}.`;
-// }
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
 
-// changeMurderer();
-// const verdict = declareMurderer();
-// console.log(verdict);
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
 
 // I believe the murderer would be Mr Green as both plotTwist and unexpectedOutcome are only visible inside the block code and as such cannot be referenced in the next set of curly brackets.
 
